e919a22ab928faf56d40647fbe78537fcc9c3765
ali-ashraff <ali.ashraf.200404@gmail.com>
2025-04-24 17:07:38 +0200

    second commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   API_KEY: "054eeeaafc094f89b3e124928252104",
-  BASE_URL: "http://api.weatherapi.com/v1",
+  BASE_URL: "https://api.weatherapi.com/v1",
   ENDPOINTS: {
     FORECAST: "/forecast.json",
     SEARCH: "/search.json",
